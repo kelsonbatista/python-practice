3c808a658559b3fe6d7b6f6da0c6d4a2b368428a
kelsonbatista <kelsonbatista@gmail.com>
2022-09-07 15:22:51 -0300

    feat: exercise1

---
+++ exercise1.py
@@ -0,0 +1,19 @@
+# arithmetic
+
+# Exercício 1: No terminal, inicialize duas variáveis a e b,
+# sendo a = 10 e b = 5. Mostre o resultado das 7 operações básicas
+# (soma, subtração, multiplicação, divisão, divisão inteira,
+# potenciação e módulo) envolvendo essas variáveis.
+
+a = 20
+b = 5
+
+print(a + b, '<<<< a + b')
+print(a - b, '<<<< a - b')
+print(a / b, '<<<< a / b')
+print(a * b, '<<<< a * b')
+# shows the rest of division
+print(a % b, '<<<< a % b - rest of division')
+# shows the integer of division
+print(a // b, '<<<< a // b - integer of division')
+print(a ** b, '<<<< a ^ b')
