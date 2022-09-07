caf9b373d2aeee8147182501f8b849060c32d685
kelsonbatista <kelsonbatista@gmail.com>
2022-09-07 17:23:52 -0300

    feat: exercise14

---
+++ exercise14.py
@@ -0,0 +1,11 @@
+# Exercício 14: Percorra a lista do exercício 13 e imprima
+# "Múltiplo de 3" se o elemento for divisível por 3.
+
+ratings = [6, 8, 5, 9, 10]
+
+new_ratings2 = [10 * rating for rating in ratings]
+print(new_ratings2, '<<<< new_ratings 2')
+
+for value in new_ratings2:
+    if (value % 3 == 0):
+        print(f'{value} <<<< multiplo de 3')
