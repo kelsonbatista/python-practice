e97e8ae40994c2962629e52a46fd1a8dbcc18c62
kelsonbatista <kelsonbatista@gmail.com>
2022-09-07 15:48:31 -0300

    feat: exercise5

---
+++ exercise5.py
@@ -0,0 +1,13 @@
+trybe_course = ["Introdução", "Front-end", "Back-end"]
+
+# Exercício 5: Adicione o elemento "Ciência da Computação" à lista.
+
+trybe_course.append("Computer Science")
+
+print(trybe_course, '<<<<<<')
+
+# Exercício 6: Acesse e altere o primeiro elemento da lista para "Fundamentos".
+
+trybe_course[0] = "Fundamentals"
+
+print(trybe_course)
