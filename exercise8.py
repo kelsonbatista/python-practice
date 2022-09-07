b0aa1d37430158688575adba175204d9c6b20454
kelsonbatista <kelsonbatista@gmail.com>
2022-09-07 16:11:45 -0300

    feat: exercise8

---
+++ exercise8.py
@@ -0,0 +1,18 @@
+info = {
+  "personagem": "Margarida",
+  "origem": "Pato Donald",
+  "nota": "Namorada do personagem principal nos quadrinhos do Pato Donald",
+}
+
+# Exercício 8: Insira no objeto uma nova propriedade com o
+# nome de chave "recorrente" e o valor "Sim". Em seguida,
+# imprima o objeto no console.
+
+info["recorrente"] = "Sim"
+print(info)
+
+# Exercício 9: Remova a propriedade cuja chave é "origem"
+# e imprima o objeto no console.
+
+del info["origem"]
+print(info)
