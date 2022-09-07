4f3808fc8556ea238c739faf7e4bdd81c17a04bc
kelsonbatista <kelsonbatista@gmail.com>
2022-09-07 16:45:56 -0300

    feat: exercise10

---
+++ exercise10.py
@@ -0,0 +1,16 @@
+# Exercício 10: Após uma consulta do banco de dados, temos linhas
+# que contém nome, sobrenome e idade como: "Thiago", "Nobre", 29.
+# Que estrutura vista anteriormente seria recomendada dado que
+# após esta consulta somente exibimos estes valores?
+
+# A estrutura recomendada é a tuple. Caso houvesse necessidade de
+# editar os valores ou adicionar mais valores, usaríamos uma list.
+
+# Exercício 11: Realizar a contagem de quantas vezes cada elemento
+# aparece em uma sequência é uma técnica muito útil na solução de
+# alguns problemas. Qual é a estrutura mais recomendada para o
+# armazenamento desta contagem?
+
+# dict é a estrutura mais adequada, pois conseguimos armazenar o
+# elemento da lista como chave e a quantidade de vezes que
+# ele aparece como valor da chave.
