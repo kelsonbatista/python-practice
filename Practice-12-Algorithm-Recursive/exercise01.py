16ef9ab8569b2b363e760ed80f31272a629c70c3
kelsonbatista <kelsonbatista@gmail.com>
2022-10-08 20:11:24 -0300

    feat: exercise01 recursive

---
+++ Practice-12-Algorithm-Recursive/exercise01.py
@@ -0,0 +1,13 @@
+# Exercício 1: Crie um algoritmo não recursivo para contar quantos
+# números pares existem em uma sequência numérica (1 a n).
+
+
+def count_even_numbers(n):
+    count = 0
+    for i in range(1, n + 1):
+        if i % 2 == 0:
+            count += 1
+    return count
+
+
+print(count_even_numbers(10))
