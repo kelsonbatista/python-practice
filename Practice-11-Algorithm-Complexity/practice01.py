1a353ec1d5a8e1d4bd08c35fff24eec1e6791c8a
kelsonbatista <kelsonbatista@gmail.com>
2022-10-07 22:31:49 -0300

    feat: practice01

---
+++ Practice-11-Algorithm-Complexity/practice01.py
@@ -0,0 +1,17 @@
+def multiply_arrays(array1, array2):
+    result = []
+    number_of_iterations = 0
+
+    for number1 in array1:
+        for number2 in array2:
+            print(f'Array 1 - {number1}:{number2} - Array 2')
+            result.append(number1 * number2)
+            number_of_iterations += 1
+
+    print(f'{number_of_iterations} iterações!')
+    return result
+
+
+meu_array = [1, 2, 3, 4, 5]
+
+multiply_arrays(meu_array, meu_array)
